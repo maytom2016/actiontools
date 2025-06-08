fcc3e00e5d713bdc5218149b3e71db1456c437ce
maytom2016 <qwerty448qwe@163.com>
2025-06-08 20:18:57 +0800

    Update check_el.c

--- check_el.c
+++ check_el.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
-#include <stdint.h>
+// #include <stdio.h>
+// #include <stdint.h>
 
+// int main() {
+//     uint64_t el;
+//     asm volatile("mrs %0, CurrentEL" : "=r"(el));
+//     printf("当前 Exception Level: EL%llu\n", el >> 2);
+//     return 0;
+// }
+
+#include <stdio.h>
 int main() {
-    uint64_t el;
-    asm volatile("mrs %0, CurrentEL" : "=r"(el));
-    printf("当前 Exception Level: EL%llu\n", el >> 2);
+    printf("Test program running\n");
     return 0;
 }
