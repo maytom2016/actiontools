46161ea8e3974459ba85c54088aa6fe32a00f3cc
maytom2016 <qwerty448qwe@163.com>
2025-06-08 20:23:10 +0800

    Update check_el.c

--- check_el.c
+++ check_el.c
@@ -1,15 +1,25 @@
-// #include <stdio.h>
-// #include <stdint.h>
-
-// int main() {
-//     uint64_t el;
-//     asm volatile("mrs %0, CurrentEL" : "=r"(el));
-//     printf("当前 Exception Level: EL%llu\n", el >> 2);
-//     return 0;
-// }
-
 #include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+#include <setjmp.h>
+
+static sigjmp_buf jmpbuf;
+
+void handle_sigill(int sig) {
+    siglongjmp(jmpbuf, 1);
+}
+
 int main() {
-    printf("Test program running\n");
+    uint64_t el;
+    
+    signal(SIGILL, handle_sigill);
+    
+    if (sigsetjmp(jmpbuf, 1) {
+        printf("当前在用户模式 (EL0)\n");
+    } else {
+        asm volatile("mrs %0, CurrentEL" : "=r"(el));
+        printf("当前 Exception Level: EL%llu\n", el >> 2);
+    }
+    
     return 0;
 }
